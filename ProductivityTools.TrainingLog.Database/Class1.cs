2ba6791efd84fd2fedbab0815de608b377335584
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-19 23:07:29 +0100

    context

--- ProductivityTools.TrainingLog.Database/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ProductivityTools.TrainingLog.Database
-{
-    public class Class1
-    {
-    }
-}
---
+++ ProductivityTools.TrainingLog.Database/TrainingDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ProductivityTools.TrainingLog.Contract;
+using System;
+
+namespace ProductivityTools.TrainingLog.Database
+{
+    public class TrainingDbContext: DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public TrainingDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+                   builder.AddConsole()
+                          .AddFilter(DbLoggerCategory.Database.Command.Name,
+                                     LogLevel.Information));
+            return serviceCollection.BuildServiceProvider()
+                    .GetService<ILoggerFactory>();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("Meetings"));
+                optionsBuilder.UseLoggerFactory(GetLoggerFactory());
+                optionsBuilder.EnableSensitiveDataLogging();
+                base.OnConfiguring(optionsBuilder);
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("mt");
+           // modelBuilder.Entity<Training>().HasKey(x => x.TrainingId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
