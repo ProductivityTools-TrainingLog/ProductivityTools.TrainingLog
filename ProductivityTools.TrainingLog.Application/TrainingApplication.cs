011c0be90fdf29206d2ba6d966e75653008a63c5
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-24 18:10:03 +0100

    schema of row processing

--- ProductivityTools.TrainingLog.Application/TrainingApplication.cs
+++ ProductivityTools.TrainingLog.Application/TrainingApplication.cs
@@ -21,8 +21,8 @@ namespace ProductivityTools.TrainingLog.Application
 
         public string AddRaw(TrainingRaw training)
         {
-            var x=this.Context.TrainingRaw.ToList();
-            var t=this.Context.TrainingRaw.SingleOrDefault(x => x.Start == training.Start && x.End == training.End && x.Sport == training.Sport);
+            var x = this.Context.TrainingRaw.ToList();
+            var t = this.Context.TrainingRaw.SingleOrDefault(x => x.Start == training.Start && x.End == training.End && x.Sport == training.Sport);
             if (t == null)
             {
                 this.Context.TrainingRaw.Add(training);
@@ -37,6 +37,21 @@ namespace ProductivityTools.TrainingLog.Application
 
         public void ETL()
         {
+            var trainingRaws = this.Context.TrainingRaw.Where(x => x.Processed == false);
+            foreach (var trainingRaw in trainingRaws)
+            {
+                var record = this.Context.Training.Any(x => x.TrainingId == trainingRaw.TrainingId);
+                if (record == false)
+                {
+                    ProcessRecord(trainingRaw);
+                }
+                
+            }
+        }
+
+        private void ProcessRecord(TrainingRaw trainingRaw)
+        {
+            Training training = new Training();
             
         }
     }
